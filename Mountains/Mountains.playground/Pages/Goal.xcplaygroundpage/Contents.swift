77cd8ab5d7335e013b0fb557281acb1a29a6a942
Student <student@macbook-pro-fiset-algarvio.local>
2017-02-02 16:00:33 -0500

    I made the big mountains

--- Mountains/Mountains.playground/Pages/Goal.xcplaygroundpage/Contents.swift
+++ Mountains/Mountains.playground/Pages/Goal.xcplaygroundpage/Contents.swift
@@ -20,11 +20,18 @@ let canvas = Canvas(width: 300, height: 300)
 canvas.defaultLineWidth = 1
 
 // Makes a grid of dots.
-for y in stride(from: 300, through: 0, by: -25) {
+for y in stride(from: 300, through: 150, by: -25) {
     for x in stride(from: 0, through: 300, by: 25) {
         canvas.drawEllipse(centreX: x, centreY: y, width: 1, height: 1)
     }
 }
+
+// Make the Mountains
+canvas.drawLine(fromX: 26, fromY: 175, toX: -76, toY: 75, lineWidth: 71)
+canvas.drawLine(fromX: 126, fromY: 175, toX: 26, toY: 75, lineWidth: 71)
+canvas.drawLine(fromX: 226, fromY: 175, toX: 126, toY: 75, lineWidth: 71)
+canvas.drawLine(fromX: 326, fromY: 175, toX: 226, toY: 75, lineWidth: 71)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 300, height: 150)
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
 PlaygroundPage.current.liveView = canvas.imageView
